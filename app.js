eec34110e0a31efd54af8804ec76606211657cab
Brannybk <branny.dev@gmail.com>
2019-05-13 05:58:20 -0500

    eslint with airbnb standard extends

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ const debug = require('debug')('nodejs-base:main');
 
 debug.log = console.log.bind(console);
 
-debug('hello world! %O', {one: 34, two: 'dos'});
+debug('hello world! %O', { one: 34, two: 'dos' });
